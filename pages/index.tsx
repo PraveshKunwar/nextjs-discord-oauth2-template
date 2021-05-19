e0373788b1c77e26c8941eca444b292adcec49de
Pravesh Kunwar <praveshk04@outlook.com>
2021-05-19 12:38:30 -0400

    feat: initial commmits!

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import {NextPage} from "next";
+
+const Index: NextPage = (): JSX.Element => {
+  return (
+    <div className="nextjs-oauth2-discord-template">
+      Hello World!
+    </div>
+  )
+}
+
+export default Index;
