0102a122cae5ab391fd8b2124d75cd761a789af8
Pravesh Kunwar <praveshk04@outlook.com>
2021-05-19 13:26:41 -0400

    feat: adding api requests.

--- pages/api/auth.tsx
+++ pages/api/auth.tsx
@@ -1,5 +1,11 @@
 import {NextApiResponse, NextApiRequest} from "next";
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
-  res.status(200).send("Hello World!");
+  res.redirect(
+  		`https://discord.com/api/oauth2/authorize?client_id=${
+  		    "CLIENT_ID_GOES_HERE"
+  		}&redirect_uri=${encodeURIComponent(
+  			'http://localhost:3000/api/authorized'
+  		)}&response_type=code&scope=${encodeURIComponent('identify guilds email')}`
+  	);
 }
